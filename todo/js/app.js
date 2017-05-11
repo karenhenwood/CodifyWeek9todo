388b837aae3cd84d0d7428f6367735d21a768fd1
karenhenwood <karenhenwood@gmail.com>
2017-05-10 21:42:59 -0700

    add comments, CSS

--- todo/js/app.js
+++ todo/js/app.js
@@ -1,19 +1,19 @@
 
 document.getElementById('add').onclick = function(){
-//get item and add it to the list
+//get item and add it to the list with a delete button included
   var item = document.getElementById('input').value;
   var li = document.createElement('li');
-  li.innerHTML = item + "  <button class='delete'>remove</button>";
+  li.innerHTML = item + "  <button class='btn delete'>remove</button>";
   var ol = document.getElementById('list');
   ol.appendChild(li);
   //clear input
   document.getElementById('input').value = "";
-  //add event listeners to list item buttons
+  //call function add event listeners to list item buttons
   addListener();
-  console.log(item)
 };
 
-
+//cycle through the list at each addition and make the delete buttons respond to clicks by calling
+//the remove function
 function addListener(){
   var deletables = document.getElementsByClassName("delete");
   for (var i = 0; i < deletables.length; i++) {
@@ -21,6 +21,7 @@ function addListener(){
   }
 }
 
+//when the delete buttons are clicked, find the list and the item, then delete the item as a child of the list
 function remove(){
   var list = this.parentNode.parentNode
   var item = this.parentNode;
